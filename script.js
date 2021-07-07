5e31b31d5c1dc1e7e50de3021ec41acfbded0a3d
Maciej Sawicki <macieksawickim@gmail.com>
2021-07-07 22:15:24 +0200

    add scrollFix when navigation is active

--- script.js
+++ script.js
@@ -17,6 +17,49 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+
 
 // NAVIGATION
 const navSlide = () => {
@@ -30,7 +73,17 @@ const navSlide = () => {
     burger.classList.toggle("toggle");
   };
 
+  const scrollFix = () => {
+    if (burger.classList.contains("toggle")) {
+      disableScroll();
+    }
+    else {
+      enableScroll();
+    }
+  }
+
   burger.addEventListener("click", menuFunction);
+  burger.addEventListener("click", scrollFix);
 };
 
 navSlide();
@@ -57,6 +110,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   // if
 });
+
 
 // FAQS CONTAINERS
 
